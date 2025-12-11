917ae3cd5aa23a0699794556dc03c64c5e905197
AncheJeez <andresjsangon@gmail.com>
2025-12-11 14:38:20 +0100

    cambiado cosas cliente principal

--- cliente_principal/script.js
+++ cliente_principal/script.js
@@ -2,5 +2,6 @@ const sidebar = document.getElementById('sidebar');
 const profileButton = document.getElementById('profile-button');
 
 profileButton.addEventListener('click', () => {
-    sidebar.classList.add('active');
+    // sidebar.classList.add('active');
+    console.log("prueba");
 });
