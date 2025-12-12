4067b4a9cfed6b4ae74f2a74b03500df5564c683
AncheJeez <andresjsangon@gmail.com>
2025-12-12 14:39:08 +0100

    añadido sidebar al cliente

--- cliente_principal/script.js
+++ cliente_principal/script.js
@@ -1,7 +1,14 @@
 const sidebar = document.getElementById('sidebar');
-const profileButton = document.getElementById('profile-button');
+// const profileButton = document.getElementById('profile-button');
 
-profileButton.addEventListener('click', () => {
-    // sidebar.classList.add('active');
-    console.log("prueba");
-});
+// profileButton.addEventListener('click', () => {
+//     sidebar.classList.add('active');
+// });
+
+document.getElementById("profile-button").onclick = () => {
+    sidebar.classList.toggle('active');
+};
+
+document.getElementById("back-button").onclick = () => {
+    sidebar.classList.toggle('active');
+}
