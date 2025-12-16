b9cc0d2849779c5c3f3bca2e8896ea8f0761d9a7
AncheJeez <andresjsangon@gmail.com>
2025-12-16 10:57:50 +0100

    hecho login en juegos reunidos y cambiado svg de sidebar

--- cliente_principal/script.js
+++ cliente_principal/script.js
@@ -1,14 +1,42 @@
+const listaUsers = [{user: "admin", password: "admin"},{user: "andres", password: "andres"},{user: "testing", password: "testing"}];
+
+//sidebar
 const sidebar = document.getElementById('sidebar');
-// const profileButton = document.getElementById('profile-button');
-
-// profileButton.addEventListener('click', () => {
-//     sidebar.classList.add('active');
-// });
-
 document.getElementById("profile-button").onclick = () => {
     sidebar.classList.toggle('active');
 };
 
 document.getElementById("back-button").onclick = () => {
     sidebar.classList.toggle('active');
+}
+//formularip
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+    const givenUsername = document.getElementById("usuario").value;
+    const givenPassword = document.getElementById("password").value;
+    var flag = false;
+    for(var i=0, count = listaUsers.length; i< count; i++){
+        console.log("test1");
+        if(listaUsers[i].user == givenUsername && listaUsers[i].password == givenPassword){
+            console.log("test2");
+            localStorage.setItem("user", givenUsername);
+            localStorage.setItem("password", givenPassword);
+            flag = true;
+            alert("Usuario correcto, está guardados los datos localmente");
+            break;
+        }
+    }
+    if(!flag){
+        alert("Usuario o contraseña no existe");
+    }
+});
+
+//borrar sesion
+document.getElementById("close-session-button").onclick = () => {
+    localStorage.removeItem("user");
+    localStorage.removeItem("password");
+}
+//borrar datos almacenados
+document.getElementById("borrar-datos-almacenados-button").onclick = () => {
+    localStorage.clear();
 }
